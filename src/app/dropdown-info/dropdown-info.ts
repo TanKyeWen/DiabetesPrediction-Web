fca3536193ae1077244562ef4d9449ff0cdbd615
TanKyeWen <tkyewen@gmail.com>
2025-09-22 13:22:39 +0800

    completed dropdown

--- src/app/dropdown-info/dropdown-info.ts
+++ src/app/dropdown-info/dropdown-info.ts
@@ -1,13 +1,20 @@
-import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dropdown-info',
-  imports: [NgIf],
+  imports: [CommonModule],
   templateUrl: './dropdown-info.html',
   styleUrls: ['./dropdown-info.css'],
   standalone: true
 })
 export class DropdownInfo {
-  isOpen = false;
+  showContent = false;
+
+  toggleDropdown() {
+    this.showContent = !this.showContent;
+  }
+
+  @Input() contentHeader: string = 'No Content'
+  @Input() contentDetails: string = 'No Content'
 }
