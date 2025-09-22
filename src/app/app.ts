db9891aa4098f5881dd4546180b88a3a093591c9
TanKyeWen <tkyewen@gmail.com>
2025-09-22 13:29:37 +0800

    edited dropdown animation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DropdownInfo } from './dropdown-info/dropdown-info';
+import { CardInfo } from './card-info/card-info';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, DropdownInfo],
+  imports: [RouterOutlet, DropdownInfo, CardInfo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
